4d69fd0439eb262a80e33775c2de6f03b7ecea8d
JeanPilous <theo_rougier@hotmail.fr>
2021-11-03 11:57:42 +0100

    add strapi

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'db4cbfe67eff033d7237556b65e1107e'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '6039f8c9-0272-4572-bcea-215b10b09441'
+};
